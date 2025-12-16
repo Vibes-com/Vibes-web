1509a344286e714a606967d34810c014e01530d8
Ayushman <asolanki789@gmail.com>
2025-12-16 17:47:08 +0530

    feat merge conflicts

--- app/components/technologies/web-development/WebDevelopment.tsx
+++ app/components/technologies/web-development/WebDevelopment.tsx
@@ -1,9 +1,19 @@
+
 import React from 'react'
+import WebDevelopmentHero from './web-development-hero/WebDevelopmentHero'
+import WebDevelopmentAbout from './web-development-about/WebDevelopmentAbout'
+import WebDevelopmentWorkflow from './web-development-workflow/WebDevelopmentWorkflow'
+import WhyChooseVibes from './why-choose-vibes/WhyChooseVibes'
 
 const WebDevelopment = () => {
-  return (
-    <div>web development</div>
-  )
+    return (
+        <div>
+            <WebDevelopmentHero />
+            <WebDevelopmentAbout />
+            <WebDevelopmentWorkflow />
+            <WhyChooseVibes />
+        </div>
+    )
 }
 
 export default WebDevelopment
