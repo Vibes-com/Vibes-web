e5392ff9ed578a50b298acfad7953564356893ad
Ayushman <asolanki789@gmail.com>
2025-12-17 17:19:33 +0530

    feat add success story in tech

--- app/components/technologies/web-development/WebDevelopment.tsx
+++ app/components/technologies/web-development/WebDevelopment.tsx
@@ -1,12 +1,16 @@
-
+"use client";
 import WebDevelopmentHero from './web-development-hero/WebDevelopmentHero'
 import WebDevelopmentAbout from './web-development-about/WebDevelopmentAbout'
 import WebDevelopmentWorkflow from './web-development-workflow/WebDevelopmentWorkflow'
 import WhyChooseVibes from './why-choose-vibes/WhyChooseVibes'
 import TechnologyStack from './technology-stack/TechnologyStack'
 import BuiltWay from './built-way/BuiltWay'
+import SuccessStory from '../../technology-details/success-story/SuccessStory'
+import { useRouter } from "next/navigation";
+
 
 const WebDevelopment = () => {
+  const router = useRouter();
     return (
         <div>
             <WebDevelopmentHero />
@@ -15,6 +19,14 @@ const WebDevelopment = () => {
             <WhyChooseVibes />
             <TechnologyStack />
             <BuiltWay />
+            <SuccessStory
+              title="Let’s Build Something Powerful"
+              subtitle="Scalable digital solutions tailored for you"
+              buttonText="Talk to Experts"
+              arrowImage="/assests/img/technology-details/arrow.png"
+              className="bg-black"
+              onButtonClick={() => router.push("/contact")}
+              />
         </div>
     )
 }
