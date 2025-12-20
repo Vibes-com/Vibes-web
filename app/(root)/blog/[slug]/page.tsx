c9a4f806b8baee4f9545544c0f0c236bb6859f7f
Raj <rajdesignden@gmail.com>
2025-12-20 16:35:15 +0530

    Updated Blog Page

--- app/(root)/blog/[slug]/page.tsx
+++ app/(root)/blog/[slug]/page.tsx
@@ -18,19 +18,24 @@ export default function BlogDetailPage({ params }: PageProps) {
   const blog = data?.data;
 
   return (
-    <section className="max-w-4xl mx-auto py-10 px-4">
-      {/* Blog Title */}
-      <h1 className="text-3xl font-bold mb-6">
-        {blog?.blog_title}
-      </h1>
-
-      {/* Blog Description (HTML) */}
-      <div
-        className="prose max-w-none"
-        dangerouslySetInnerHTML={{
-          __html: blog?.blog_description || "",
-        }}
-      />
+    <section className="w-full">
+      <div className="relative">
+        <img className="w-full" src={blog?.banner_img} />
+        {/* Blog Title */}
+        <h1 className="text-5xl w-[80%] font-medium uppercase mb-6 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 p-4 text-center text-white z-10">
+          {blog?.blog_title}
+        </h1>
+      </div>
+      <div className="max-w-4xl mx-auto pb-20 pt-10 px-4 blog-content">
+        <h2 className="!mt-0">Overview</h2>
+        {/* Blog Description (HTML) */}
+        <div
+          className="prose max-w-none"
+          dangerouslySetInnerHTML={{
+            __html: blog?.blog_description || "",
+          }}
+        />
+      </div>
     </section>
   );
 }
