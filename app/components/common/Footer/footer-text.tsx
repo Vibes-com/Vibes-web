04debdba1c2eeb525cdd64b3251175175d889dad
Raj <rajdesignden@gmail.com>
2026-01-05 16:51:40 +0530

    content corrected

--- app/components/common/Footer/footer-text.tsx
+++ app/components/common/Footer/footer-text.tsx
@@ -4,8 +4,8 @@ import React from 'react'
 const FooterText = () => {
   return (
     <div>
-      <h5 className=" text-[#F6F6F6]  font-medium text-[28px] sm:text-[40px] md:text-[40px] lg:text-[40px] xl:text-[40px] 2xl:text-[40px] leading-[100%] tracking-normal text-white">Stay Connected</h5>
-      <p className=" mt-4 text-[#B3B3B3]  font-normal text-[13px] leading-[25px] tracking-normal">Subscribe to receive news and updates from our digital initiatives.</p>
+      <h5 className=" text-[#F6F6F6]  font-medium text-[28px] sm:text-[40px] md:text-[40px] lg:text-[40px] xl:text-[40px] 2xl:text-[40px] leading-[100%] tracking-normal text-white">Stay Ahead</h5>
+      <p className=" mt-4 text-[#B3B3B3]  font-normal text-[13px] leading-[25px] tracking-normal">Rooted, Relvant and Ready for new challenges.</p>
     </div>
   )
 }
