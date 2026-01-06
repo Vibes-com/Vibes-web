46ff8badc47ac80e3b975766487f9725af2a9126
akash singh <akashsingh401679@gmail.com>
2026-01-06 11:29:16 +0530

    fix nav

--- app/components/common/Navbar.tsx
+++ app/components/common/Navbar.tsx
@@ -25,14 +25,14 @@ const Navbar = () => {
       className={` fixed bg-black sm:bg-[rgba(6,6,6,0.24)] md:bg-[rgba(6,6,6,0.24)] lg:bg-[rgba(6,6,6,0.24)] xl:bg-[rgba(6,6,6,0.24)] 2xl:bg-[rgba(6,6,6,0.24)] inset-x-0 z-100 h-22 transition-all duration-300
         ${scrolled ? "md:bg-black/90 lg:bg-black/90 xl:bg-black/90 2xl:bg-black/90  backdrop-blur-sm" : "bg-[rgba(6,6,6,0.24)]"}
       `}>
-      <div className="h-full  flex items-center justify-between mx-auto px-4">
+      <div className="h-full  flex items-center justify-between mx-auto px-4 ">
         <Logo />
 
         {/* Desktop Menu */}
         <div className="flex ms-auto gap-8 items-center">
           <NavMenu className="hidden lg:block" />
 
-          <div className="flex items-center gap-3">
+          <div className="flex items-center gap-3 talk-btn">
             <a href="tel:+91-8586932861"
               className="bg-[#F4BE00] text-black px-4 text-sm py-1 flex items-center space-x-5 transition  border-[2px] border-[#F4BE00] hover:text-white rounded-full *:hover:bg-transparent"
             >
