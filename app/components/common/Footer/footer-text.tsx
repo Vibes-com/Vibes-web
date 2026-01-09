50716019490367da00fb74fb6f01aa861fc0575b
akash singh <akashsingh401679@gmail.com>
2026-01-09 12:15:25 +0530

    correct spelling footer

--- app/components/common/Footer/footer-text.tsx
+++ app/components/common/Footer/footer-text.tsx
@@ -5,7 +5,7 @@ const FooterText = () => {
   return (
     <div>
       <h5 className=" text-[#F6F6F6]  font-medium text-[28px] sm:text-[40px] md:text-[40px] lg:text-[40px] xl:text-[40px] 2xl:text-[40px] leading-[100%] tracking-normal text-white">Stay Ahead</h5>
-      <p className=" mt-4 text-[#B3B3B3]  font-normal text-[13px] leading-[25px] tracking-normal">Rooted, Relvant and Ready for new challenges.</p>
+      <p className=" mt-4 text-[#B3B3B3]  font-normal text-[13px] leading-[25px] tracking-normal">Rooted, Relevant and Ready for new challenges.</p>
     </div>
   )
 }
