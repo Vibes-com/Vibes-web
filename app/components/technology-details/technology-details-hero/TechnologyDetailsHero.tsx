07bd9ffc5b476c45d43c62c8668b5a714f6dba02
akash singh <akashsingh401679@gmail.com>
2026-02-20 15:23:13 +0530

    update mobile banner

--- app/components/technology-details/technology-details-hero/TechnologyDetailsHero.tsx
+++ app/components/technology-details/technology-details-hero/TechnologyDetailsHero.tsx
@@ -19,18 +19,18 @@ const TechnologyDetailsHero: React.FC = () => {
                             <span className="hero-highlight relative z-9 w-fit hero-heading font-poppins font-semibold text-[33px] sm:text-[65px] md:text-[35px] lg:text-[65px] xl:text-[65px] 2xl:text-[65px] leading-[116%] tracking-normal capitalize text-[#F4BE00]">Digital Solutions</span>
                         </h1>
 
-                        <p className="mt-10 font-poppins font-semibold text-[14px] sm:text-[18px] md:text-[18px] lg:text-[18px] xl:text-[18px] 2xl:text-[18px] leading-[128%] tracking-normal capitalize text-[#FFFFFF] md:max-w-[400px] lg:max-w-[500px]">
+                        <p className="mt-10 font-poppins font-semibold text-[13px] sm:text-[18px] md:text-[18px] lg:text-[18px] xl:text-[18px] 2xl:text-[18px] leading-[128%] tracking-normal capitalize text-[#FFFFFF] md:max-w-[400px] lg:max-w-[500px]">
                           Vibes builds smart, responsive digital systems and apps through advanced software-technology services designed for scalability and performance
                         </p>
                         <div className=" mt-4">
                             <Link href="/contact-for-digital-requirements">
-                                <Button2 className=" hidden md:hidden lg:flex sm:flex mt-5 text-[12px] lg:text-[18px]  p-[10px] lg:p-[20px] cursor-pointer hover:border-[#2B4C69] hover:border-[2px] hover:text-[#F4BE00] hover:bg-transparent bg-[#FFFFFF] text-[#204667]">
+                                <Button2 className=" md:hidden lg:flex sm:flex mt-0 lg:mt-5 text-[12px] lg:text-[18px]  p-[10px] lg:p-[20px] cursor-pointer hover:border-[#2B4C69] hover:border-[2px] hover:text-[#F4BE00] hover:bg-transparent bg-[#FFFFFF] text-[#204667]">
                                     <span>Create Your Edge</span>
                                     <ArrowUpIcon className="transform rotate-45" />
                                 </Button2>
                             </Link>
                         </div>
-                        <div className=" mt-9 max-w-7xl me-auto  hidden sm:flex  md:hidden lg:flex flex-col md:flex-row items-center justify-center gap-5 w-max">
+                        <div className="mt-4 lg:mt-9 max-w-7xl me-auto flex sm:flex  md:hidden lg:flex  md:flex-row items-center justify-center gap-5 w-max">
                             <div className=" flex items-center -space-x-5 me-auto">
                                 <div className="w-[35px] h-[35px] lg:w-[55px] lg:h-[55px] rounded-full border border-white/70 flex items-center justify-center shadow-[0px_4px_4px_0px_#00000040]">
                                     <img
